9d4617468fbb88b125b9493ba01501b79e32df7a
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-03-15 17:05:14 -0700

    got first test to pass yay, fixed a syntax error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('any-api routes', () => {
   beforeEach(() => {
@@ -11,15 +12,15 @@ describe('any-api routes', () => {
   afterAll(() => {
     pool.end();
   });
-});
 
-it('creates a cat', async () => {
-  const expected = {
-    name: 'Xena',
-    age: 1,
-    favoriteToy: 'Hair tie',
-  };
-  const res = await request(app).post('/api/v1/cats').send(expected);
+  it('creates a cat', async () => {
+    const expected = {
+      name: 'Xena',
+      age: 1,
+      favoriteToy: 'Hair tie',
+    };
+    const res = await request(app).post('/api/v1/cats').send(expected);
 
-  expect(res.body).toEqual({ id: expect.any(String), ...expected });
+    expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
 });
